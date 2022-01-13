1eb7ab9988ff6dd27051199b475b3ec4e8500baf
Paulina Stadnik <paulina.stadnik@rover.com>
2022-01-13 23:28:43 +0100

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => {return a-b})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) return this.items[pos]
+    else {
+      throw new OutOfBounds("OutOfBounds")
+    } 
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0
+    return this.items.reduce((acc, a) => acc + a)
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) throw new Error('EmptySortedList')
+    return (this.items.reduce((acc, a) => acc + a))/this.items.length
+  }
 }
 
+new SortedList
+
 module.exports = SortedList;
+
